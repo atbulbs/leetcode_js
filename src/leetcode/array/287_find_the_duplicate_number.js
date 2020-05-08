d4dcdf18751530be2c127c3ed271f852726672c6
liaoxiaomin <liaoxiaomin@vipkid.com.cn>
2020-05-08 10:37:50 +0800

    287_find_the_duplicate_number

--- src/leetcode/array/287_find_the_duplicate_number.js
+++ src/leetcode/array/287_find_the_duplicate_number.js
@@ -8,13 +8,19 @@ var findDuplicate = function(nums) {
   while (true) {
     slowPointer = nums[slowPointer]
     fastPointer = nums[nums[fastPointer]]
+    console.warn('slowPointer -> ' + slowPointer)
+    console.warn('fastPointer -> ' + fastPointer + '\n')
     if (slowPointer == fastPointer) {
       let _slowPointer = 0
       while (nums[_slowPointer] !== nums[slowPointer]) {
         slowPointer = nums[slowPointer]
         _slowPointer = nums[_slowPointer]
       }
+      console.warn('入环节点的索引:', nums[_slowPointer])
+      console.warn('入环节点的值:', nums[nums[_slowPointer]])
       return nums[_slowPointer]
     }
   }
 };
+
+findDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 5])
